2659e6a25f210419faef0c7e03dc52bcfa29fbd5
Alexander Vishnevskiy <alexander.vishnevskiy@cerebras.net>
2024-05-05 01:10:52 -0700

    Add more metrics

--- lm_eval/tasks/cerebras_v3/utils.py
+++ lm_eval/tasks/cerebras_v3/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def get_options(doc):
     options = [str(option) for option in doc['options']]
     return options
@@ -13,3 +16,31 @@ def doc_to_text(doc):
     options_str = ''.join([f'{op[0]}. {op[1]}\n' for op in options])
     prompt = f"{doc['question'].strip()}\n{options_str}Answer:"
     return prompt
+
+def process_results(doc, results):
+    options_letters = ['A', 'B', 'C', 'D']
+    ans_id = options_letters.index(doc['answer'])
+    
+    logprobs = [res[0] for res in results]
+    answer_log = logprobs[ans_id]
+    answer_perplexity = np.exp(-answer_log)
+    pred_indx = np.argmax(logprobs)
+    
+    results.sort(key=lambda x: -x[0])
+    acc = int(pred_indx == ans_id)
+    
+    target_logprob = logprobs[0]
+    if logprobs[0] == answer_log:
+        target_logprob = logprobs[1]
+        
+    margin = np.exp(-target_logprob)-answer_perplexity
+    res = {
+        'acc': acc,
+        'perplexity': answer_perplexity,
+    }
+    
+    if margin >= 0:
+        res['pos_margin'] = margin
+    else:
+        res['neg_margin'] = -margin
+    return res
